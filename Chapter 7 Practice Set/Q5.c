7297f26b0a1dc2ed39b920b8d7dc37cb813115dd
Utkarsha Dongarjal <154047293+utkarshad21@users.noreply.github.com>
2025-05-25 18:45:18 +0530

    Update Q5.c

--- Chapter 7 Practice Set/Q5.c
+++ Chapter 7 Practice Set/Q5.c
@@ -3,11 +3,16 @@ Q5. Write a program containing a function which reverses the array passed to it.
 */
 #include <stdio.h>
 
-int reverse(int [], int);
+void reverse(int [], int);
 void printArray(int [],int);
 
-int reverse(int arr[], int n){
-    
+void reverse(int arr[], int n){
+    int temp;
+    for (int i = 0; i < n / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
 }
 void printArray(int a[], int n){
     for (int i = 0; i < n; i++)
@@ -18,8 +23,15 @@ void printArray(int a[], int n){
 }
 int main(){
     int arr[] = {9, 7, 5, 0, 8, 2, 1, 6, 0};
-    printArray(arr, 10);
-    reverse(arr, 10);
-    printArray(arr, 10);
+    int n = sizeof(arr) / sizeof(arr[0]); 
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    reverse(arr, n);
+
+    printf("Reversed array: ");
+    printArray(arr, n);
+
     return 0;
 }
